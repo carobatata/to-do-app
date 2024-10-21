2cb3cf856fb0fc82af6a4d6b9ce494864d2d9972
Carolina Acosta <carolinaacosta@Carolinas-MacBook-Pro.local>
2024-10-21 09:46:50 +0200

    add scaffold to UI

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -3,13 +3,45 @@ package com.example.todoapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.example.todoapp.ui.screens.ToDoListScreen
 
+@OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ToDoListScreen()
+            Scaffold(
+                topBar = {
+                    TopAppBar(
+                        title = { Text(stringResource(R.string.app_name_scaffold)) },
+                        colors = topAppBarColors(
+                            containerColor = MaterialTheme.colorScheme.primaryContainer,
+                            titleContentColor = MaterialTheme.colorScheme.primary,
+                        )
+                    )
+                }
+            ) { innerPadding ->
+                Column(
+                    modifier = Modifier
+                        .padding(innerPadding)
+                        .fillMaxSize()
+                        .padding(25.dp)
+                ) {
+                    ToDoListScreen()
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/example/todoapp/ui/screens/ToDoListScreen.kt
+++ app/src/main/java/com/example/todoapp/ui/screens/ToDoListScreen.kt
@@ -1,14 +1,10 @@
 package com.example.todoapp.ui.screens
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.todoapp.ui.TodoViewModel
 import com.example.todoapp.ui.model.TodoItemUiState
 import org.koin.androidx.compose.getViewModel
@@ -22,11 +18,7 @@ fun ToDoListScreen() {
         viewModel.getAllTodoItems()
     }
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(25.dp)
-    ) {
+    Column {
         when (todoItems.value) {
             is TodoItemUiState.Success -> {
                 (todoItems.value as TodoItemUiState.Success).todoItemUI.forEach { item ->
